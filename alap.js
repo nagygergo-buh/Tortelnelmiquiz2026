b305d367cfe89e959a16799f79ba3db9dd83397b
Sajat Nev <email@pelda.hu>
2026-03-25 08:21:19 +0100

    kep 12 db

---
+++ alap.js
@@ -0,0 +1,10 @@
+let sz=""
+for (let i =0; i<3;i++){
+    for (let j = 0; j < 4; j++) {
+        sz+=`
+        <img src="a0.jpg" alt="kep"
+        `
+    }
+    sz+="<br>"
+}
+document.getElementById('kepekHelye').innerHTML=sz
