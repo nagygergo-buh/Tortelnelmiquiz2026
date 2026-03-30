a32761f54ebec4f61b8ce8807b0d4cc8a79c84a5
Sajat Nev <email@pelda.hu>
2026-03-30 08:07:40 +0200

    0330

--- alap.js
+++ alap.js
@@ -1,10 +1,51 @@
 let sz=""
-for (let i =0; i<3;i++){
+let sorszam=0
+for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 4; j++) {
         sz+=`
-        <img src="a0.jpg" alt="kep"
+        <img src="kepek/a${sorszam}.jpg" alt="kep" class="kisKep" onclick="nagyit(${sorszam})">
         `
+        sorszam++
     }
     sz+="<br>"
 }
-document.getElementById('kepekHelye').innerHTML=sz
+document.getElementById("kepekHelye").innerHTML=sz
+
+function nagyit(szam){
+    let sz=""
+    sz+=`
+    <img src="kepek/a${szam}.jpg" alt="">
+    `
+    document.getElementById('nagyKep').innerHTML=sz
+
+    //keverés
+    let szavakTomb=[]
+    szavakTomb.push(nevekTomb[szam].megoldas)
+    szavakTomb.push(nevekTomb[szam].tipp1)
+    szavakTomb.push(nevekTomb[szam].tipp2)
+    szavakTomb.push(nevekTomb[szam].tipp3)
+
+    for (let i = 0; i < 100; i++) {
+        let r1=Math.floor(Math.random()*szavakTomb.length)
+        let r2=Math.floor(Math.random()*szavakTomb.length)
+        let csere=szavakTomb[r1]
+        szavakTomb[r1]=szavakTomb[r2]
+        szavakTomb[r2]=csere
+
+    }
+    //gombok kkirása
+
+
+    let gombok=""
+    for (let i = 0; i < 4; i++) {
+        gombok+=`
+        <button onclick="ertekel('${szavakTomb[i]}')">${szavakTomb[i]}</button>
+        `
+
+    }
+    document.getElementById('gombokHelye').innerHTML=gombok
+
+    function ertekel(szemely){
+        alert(szemely)
+    }
+}
